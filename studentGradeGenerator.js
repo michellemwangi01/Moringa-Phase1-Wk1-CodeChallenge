d47e95a68445df3d521f26b40ad25dbe0162b0b7
michellemwangi01 <michellemwangim@gmail.com>
2023-06-16 16:52:53 +0300

    commented the program

--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -5,14 +5,18 @@
 // E - less 40.
 
 
-//***************************************/
+//declares and initializes the studentMarks variable, getting the value from a user input
 const studentmark = prompt('Please enter student studentmarks:');
+
+//A function to calculate the student grade from their marks
 function studentGrader(studentmark){
+        //An if statement that checks and ensures that the values entered are between 0 & 100, if not, user is prompted to enter valid marks
         if (studentmark >= 0 && studentmark <= 100){
+            //switch statement that checks the range for the student marks and assigns a grade depending on the marks
+            //if the marks are within the range of any of the cases, thus evaluating to TRUE, the student is assigned the grade in that Case.
             switch (true) {
                 case (studentmark > 79):
-                    console.log("Student has scored an A")
-                    
+                    console.log("Student has scored an A")//The student grade is printed out
                     break;
                 case(studentmark >= 60 && studentmark <= 79):
                     console.log("Student has scored an B")
@@ -30,9 +34,9 @@ function studentGrader(studentmark){
                     break;
             }
         }else{
-            console.log("Please enter a valid grade!")
+            console.log("Please enter a valid grade!")//Prompts the user to enter a valid grade between 0 & 100
         }
     
     
 }
-studentGrader(studentmark);
+studentGrader(studentmark);//calls the studentGrader function, passing the student mark as the argument
