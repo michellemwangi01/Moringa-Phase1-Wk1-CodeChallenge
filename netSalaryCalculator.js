49862010379e3b57112929e56e7232cfd6fc1c3a
michellemwangi01 <michellemwangim@gmail.com>
2023-06-16 13:09:59 +0300

    Added NHIF Calculator

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -34,7 +34,7 @@ function taxableIncomeCalculator(grossSalary){
     taxableIncome = grossSalary - NSSF
     return taxableIncome
 
-}//console.log(taxableIncomeCalculator(50000))
+}
 
 //MONTHLY PAYE CALCULATOR
 function payeCalculator(taxableIncomeCalculator,grossSalary){
@@ -47,15 +47,70 @@ function payeCalculator(taxableIncomeCalculator,grossSalary){
         PAYE = (24000*0.1) + ((32333 - 24000)*0.25) + ((taxableIncome-32333)*0.3)
     }
     return PAYE;
-}console.log(payeCalculator(taxableIncomeCalculator, 50000))
+}//console.log(payeCalculator(taxableIncomeCalculator, 50000))
 
 
 //MONTHLY NHIF CALCULATOR
-function NHIFDeductions() {
+function NHIFDeductions(grossSalary) {
+   
+    switch (true) {
+        case (grossSalary < 5999):
+            NHIF = 150            
+            break;
+            case (grossSalary >= 5999 && grossSalary <= 7999):
+            NHIF = 300            
+            break;
+            case (grossSalary > 7999 && grossSalary <= 11999):
+            NHIF = 400            
+            break;
+            case (grossSalary > 11999 && grossSalary <= 14999):
+            NHIF = 500            
+            break;
+            case (grossSalary > 14999 && grossSalary <= 19999):
+            NHIF = 600            
+            break;
+            case (grossSalary > 19999 && grossSalary <= 24999):
+            NHIF = 750            
+            break;
+            case (grossSalary > 24999 && grossSalary <= 29999):
+            NHIF = 850            
+            break;
+            case (grossSalary > 29999 && grossSalary <= 34999):
+            NHIF = 900            
+            break;
+            case (grossSalary > 34999 && grossSalary <= 39999):
+            NHIF = 950            
+            break;
+            case (grossSalary > 39999 && grossSalary <= 44999):
+            NHIF = 1000            
+            break;
+            case (grossSalary > 44999 && grossSalary <= 49999):
+            NHIF = 1100            
+            break;
+            case (grossSalary > 49999 && grossSalary <= 59999):
+            NHIF = 1200            
+            break;
+            case (grossSalary > 59999 && grossSalary <= 69999):
+            NHIF = 1300            
+            break;
+            case (grossSalary > 69999 && grossSalary <= 79999):
+            NHIF = 1400            
+            break;
+            case (grossSalary > 79999 && grossSalary <= 89999):
+            NHIF = 1500            
+            break;
+            case (grossSalary > 89999 && grossSalary <= 99999):
+            NHIF = 1600            
+            break;
+            case (grossSalary >= 100000):
+                NHIF = 1700
     
-
-    
+        default:
+            break;
+    }
+    return NHIF
 }
+NHIFDeductions(15890)
 
 function netSalaryCalculator(basicSalary, benefits){
     grossSalary = basicSalary + benefits
